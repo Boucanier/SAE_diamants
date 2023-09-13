5e86fa40c00614cf893815e60a2f00120247d7f1
Jules C <118428520+Boucanier@users.noreply.github.com>
2023-09-13 20:36:56 +0200

    Commentaire sur le lancement de l'application

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import algo
 import graph
 
 # By default, the program start the graphical version
-# If you want to start the CLI version, comment the line with : "version = "GRAPH" "
+# If you want to start the CLI version, uncomment the line with : "version = "GRAPH" "
+
+# Par défaut, le programme se lance dans le terminal
+# Pour lancer la version graphique il faut décommenter la ligne : "version = "GRAPH" "
 
 version = "CLI"
 # version = "GRAPH"
